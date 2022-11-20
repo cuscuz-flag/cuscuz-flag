7caf4ff41bf308c21e2960a14e49cb0c531feb37
Mario Idival <marioidival@gmail.com>
2022-11-20 20:51:52 -0300

    chore: add basic code with yew on FE projects

--- dashboard/src/main.rs
+++ dashboard/src/main.rs
@@ -1,3 +1,44 @@
+use yew::prelude::*;
+
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let link = ctx.link();
+
+        html! {
+            <section class="section">
+                <div class="container">
+                    <button class="button" onclick={link.callback(|_| Msg::AddOne)}>{ "+1"}</button>
+                    <p>{ self.value }</p>
+                </div>
+            </section>
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<Model>();
 }
