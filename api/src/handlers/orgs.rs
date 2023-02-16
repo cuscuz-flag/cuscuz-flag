660b0692393cf84590fb1988ff2d9976c2fd5755
Mario Idival <marioidival@gmail.com>
2023-02-16 20:21:37 -0300

    feat(endpoints): envs and feature flag handlers
    refs #6

--- api/src/handlers/orgs.rs
+++ api/src/handlers/orgs.rs
@@ -9,7 +9,7 @@ use validator::Validate;
 use crate::{
     error::AppError,
     repository::orgs,
-    types::{Claims, CreateOrgRequest},
+    types::{Claims, CreateOrgEnvironment, CreateOrgRequest, CreateOrgFeatureFlag},
 };
 
 pub async fn create_org(
@@ -19,12 +19,33 @@ pub async fn create_org(
 ) -> Result<impl IntoResponse, AppError> {
     org_request.validate()?;
 
-    tracing::debug!("{:?}", user);
-
     let member_id =
         Uuid::from_str(user.sub.as_str()).map_err(|e| AppError::UnexpectedError(e.to_string()))?;
 
     let org = orgs::create_org(&pool, org_request.name.unwrap().clone(), member_id).await?;
 
     Ok((StatusCode::CREATED, Json(org)))
+}
+
+pub async fn create_enviroments(
+    user: Claims,
+    State(_pool): State<PgPool>,
+    Json(env_request): Json<CreateOrgEnvironment>,
+) -> Result<impl IntoResponse, AppError> {
+    env_request.validate()?;
+
+    let member_id =
+        Uuid::from_str(user.sub.as_str()).map_err(|e| AppError::UnexpectedError(e.to_string()))?;
+
+    Ok(StatusCode::CREATED)
+}
+
+pub async fn create_feature_flag(
+    _user: Claims,
+    State(_pool): State<PgPool>,
+    Json(ff_request): Json<CreateOrgFeatureFlag>,
+) -> Result<impl IntoResponse, AppError> {
+    ff_request.validate()?;
+
+    Ok(StatusCode::CREATED)
 }
